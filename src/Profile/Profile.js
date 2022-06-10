bd1fa885740c75bc285de79d7c6ef1dd0c991595
MajorGerman <georgchannel8@gmail.com>
2022-06-10 14:04:36 +0300

    Subscribtion First Version, Little profile design changes, Profile tumbler added, Users posts in profile added!!!

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -5,7 +5,7 @@ import "./Profile.css"
 
 import { gql } from 'graphql-request';
 
-import Avatar1Img from '../img/avatars/avatar1.jpg';
+import Post from '../Post/Post';
 
 import avatar1 from '../img/avatars/avatar1.jpg';
 import avatar2 from '../img/avatars/avatar2.jpg';
@@ -19,12 +19,21 @@ function Profile (props) {
     const [avatars, setAvatars] = useState([avatar1, avatar2, avatar3, avatar4, avatar5, avatar6]);
 
     const [user, setUser] = useState({});
+    const [userPosts, setUserPosts] = useState([]);
 
     const [birthday, setBirthday] = useState(new Date());
     const [location, setLocation] = useState({});
 
     const [birthdayStyle, setBirthdayStyle] = useState("userInfo hidden");
     const [locationStyle, setLocationStyle] = useState("userInfo hidden");
+
+    const [userPostsStyle, setUserPostsStyle] = useState("profileUserPosts");
+    const [userCornerStyle, setUserCornerStyle] = useState("profileUserCorner hidden");
+    
+    const [profileTumblerPostsStyle, setProfileTumblerPostsStyle] = useState("profileTumblerPosts blue");
+    const [profileTumblerCornerStyle, setProfileTumblerCornerStyle] = useState("profileTumblerCorner");
+
+    const [subsribeText, setSubsribeText] = useState("Subscribe");
 
     const loc = useLocation();
 
@@ -35,6 +44,18 @@ function Profile (props) {
 
     const [hiddenMe, setHiddenMe] = useState("");
     const [hiddenSub, setHiddenSub] = useState("hidden");
+    const [subscribeStyle, setSubscribeStyle] = useState("hidden");
+
+    const [deleteId, setDeleteId] = useState(0);
+
+    useEffect(() => {
+        if (deleteId != -1) {
+            const newList = userPosts.filter((item) => item.id !== deleteId);
+            setUserPosts(newList);    
+            setDeleteId(-1);
+        }
+    },[deleteId])
+
 
     let query = gql`
         query GetUserById {
@@ -43,6 +64,9 @@ function Profile (props) {
                 first_name
                 last_name
                 birthday
+                subscribed {
+                    id                    
+                }
                 location {
                     id
                     city
@@ -50,11 +74,39 @@ function Profile (props) {
                     postal_code
                 }
                 avatar
+                posts {
+                    id
+                    header
+                    content
+                    author {
+                        id
+                        first_name
+                        last_name
+                        avatar
+                    }
+                    comments {
+                        id
+                        content
+                        author {
+                            id
+                            first_name
+                            last_name
+                            avatar
+                        }
+                    }
+                    likes
+                }
             }
         }
     `;
 
-    async function getData() {
+    let query2 = gql`
+        mutation AddNewSubscription {
+            addNewSubscription(user_id: 2, subscribed_id: 1)
+        }
+        `;
+
+    async function getUser() {
         try {
             return await fetch(process.env.REACT_APP_SERVER_IP, {
                 headers: {'Content-Type': 'application/json', 'verify-token': localStorage.getItem("token")},
@@ -78,20 +130,37 @@ function Profile (props) {
 
     useEffect(() =>{
         //window.history.replaceState({}, document.title);
-        getData()
+        getUser()
             .then((b) => {
                 let a = b.data.getUserById;
                 if (a.length === 0) {
                     window.location.href = "http://localhost:3000/login";
                 } else {
                     setUser(a);
+                                        
+                    setUserPosts([].concat([], a.posts));
+
                     if (a.id == localStorage.getItem("user_id")) {
                         setHiddenMe("");
                         setHiddenSub("hidden");
+                        setSubscribeStyle("hidden");
                     } else {
                         setHiddenMe("hidden");
-                        setHiddenSub("me");                        
+                        setHiddenSub("");
+                        if (a.subscribed.length == 0) {
+                            setSubsribeText("Subscribe");
+                            setSubscribeStyle("");  
+                        } else {
+                            if (a.subscribed.find((x) => x.id == localStorage.getItem("user_id")).id == localStorage.getItem("user_id")) {
+                                setSubsribeText("Unsubscribe");
+                                setSubscribeStyle("backGrey");                        
+                            } else {
+                                setSubsribeText("Subscribe");
+                                setSubscribeStyle("");  
+                            }                               
+                        }              
                     }
+
                     if (a.birthday == null) {
                         setBirthdayStyle("userInfo hidden");  
                     } else {
@@ -99,21 +168,68 @@ function Profile (props) {
                         setBirthday(b);            
                         setBirthdayStyle("userInfo");            
                     }
+
                     if (a.location == null) {
                         setLocationStyle("userInfo hidden");  
                     } else {      
                         setLocation(a.location);
                         setLocationStyle("userInfo");            
                     }
+
                 }
             })
+        
     }, [])
 
 
-    function subsribe() {
+    async function subsribe() {
+        try {
+            return await fetch(process.env.REACT_APP_SERVER_IP, {
+                headers: {'Content-Type': 'application/json', 'verify-token': localStorage.getItem("token")},
+                method: 'POST',
+                body: JSON.stringify({"query": query2})
+            }).then((a) =>{
+                return a.json()
+            }).then((b) => {
+                if (b.data.addNewSubscription) {
+                    setSubsribeText("Unsubscribe");
+                    setSubscribeStyle("backGrey");
+                } else {
+                    setSubsribeText("Subscribe");
+                    setSubscribeStyle("");
+                }
+                return b
+            })
+        } catch (err) {
+            console.log(err)
+        } 
     }
 
     function editProfile() {
+    }
+
+    function enablePosts() {
+        if (profileTumblerPostsStyle == "profileTumblerPosts") {
+
+            setProfileTumblerPostsStyle("profileTumblerPosts blue");
+            setUserPostsStyle("profileUserPosts");
+
+            setProfileTumblerCornerStyle("profileTumblerCorner");
+            setUserCornerStyle("profileUserCorner hidden");
+
+        }
+    }
+
+    function enableCorner() {
+        if (profileTumblerCornerStyle == "profileTumblerCorner") {
+
+            setProfileTumblerCornerStyle("profileTumblerCorner blue");
+            setUserCornerStyle("profileUserCorner");
+
+            setProfileTumblerPostsStyle("profileTumblerPosts");
+            setUserPostsStyle("profileUserPosts hidden");
+
+        }
     }
 
     return(
@@ -135,9 +251,33 @@ function Profile (props) {
                         </div>
                         <input className={hiddenMe} type="button" onClick={logout} value="Logout"></input>
                         <input className={hiddenMe} type="button" onClick={editProfile} value="Edit"></input>
-                        <input className={hiddenSub} type="button" onClick={subsribe} value="Subsribe"></input>
+                        <input className={subscribeStyle} type="button" onClick={subsribe} value={subsribeText}></input>
                     </div>
 
+                </div>
+
+                <div className='profileTumblerDiv'>
+                    <div className='profileTumbler'>
+                        <div onClick={enablePosts} className={profileTumblerPostsStyle}>
+                            <p> Posts 📄 </p>
+                        </div>
+                        <div style={{borderRight: "solid 0.1vw #E9E9E9", height: '30px'}}></div>
+                        <div onClick={enableCorner} className={profileTumblerCornerStyle}>
+                            <p> Corner ⭐ </p>
+                        </div>
+                    </div>
+                </div>
+                
+                <div className='profileUserPostsDiv'>
+                    <div className={userPostsStyle}>
+                        {userPosts.map((post) => <Post setDeleteId={setDeleteId} key={post.id} post={post}/>)}
+                    </div>
+                </div>
+
+                <div className='profileUserCornerDiv'>
+                    <div className={userCornerStyle}>
+                        
+                    </div>
                 </div>
 
             </div>
